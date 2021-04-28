9828338cc04d41f2c188e879be86ce2e59ecc9b1
steven615 <stevenwang615@gmail.com>
2021-04-28 15:15:31 +1000

    Add animation to test fit page

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,8 @@
 $(document).ready(() => {
-  
+  // headerAnimation();
+
+  // function headerAnimation() {
+  //   console.log('===')
+  //   $('.header').delay(100).fadeIn(500).css('display', 'flex');
+  // }
 });
