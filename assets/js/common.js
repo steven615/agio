95c97585ef2a6235eb60d0680f3d4eb4b24bae68
steven615 <stevenwang615@gmail.com>
2021-04-28 15:16:23 +1000

    Remove js

--- assets/js/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-  // headerAnimation();
-
-  // function headerAnimation() {
-  //   console.log('===')
-  //   $('.header').delay(100).fadeIn(500).css('display', 'flex');
-  // }
-});
